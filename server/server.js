f1d61f0d4ff4cb93a2034af570dd940770e421e5
unknown <marc.caraltv@gmail.com>
2018-11-16 19:54:39 +0100

    added update todos

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var {ObjectID} = require("mongodb");
 var express = require("express");
 var bodyParser = require("body-parser");
+var _ = require("lodash");
 
 var {mongoose} = require("./db/mongoose.js");
 var {User} = require("./models/user");
@@ -23,12 +24,28 @@ app.post("/todos", (req, res) => {
   })
 })
 
+app.delete("/todos/:id", (req,res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(400).send("Invalid id")
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      res.status(404).send("No todo found");
+    }
+
+    res.send(todo);
+  })
+}, (e) => {
+    res.status(400).send(e);
+})
+
 app.get("/todos", (req, res) => {
   Todo.find().then((todos) => {
-    console.log("query ok");
     res.send({todos});
   }, (e) => {
-    console.log("query ko", e);
     res.status(400).send(e);
   })
 })
@@ -50,6 +67,34 @@ app.get("/todos/:id", (req, res) => {
     res.status(400).send(e);
   })
 })
+
+app.patch("/todos/:id", (req,res) => {
+
+  var id = req.params.id;
+  var body = _.pick(req.body,["text","completed"]);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(400).send("Invalid id");
+  }
+
+  //todo what happens if there's no data to update sent to the server?
+  //if (body.length == 0) res.status(400).send("No updates sent to the server");
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completedAt = null;
+    body.completed=false;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set:body}, {new:true}).then((todo)=>{
+    res.send(todo);
+  }, (e) => {
+    res.status(400).send(e);
+  })
+
+});
+
 
 app.listen(port, () => {
   console.log(`started on port ${port}`);
